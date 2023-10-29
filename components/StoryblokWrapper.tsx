bcd93dd3d43beef527666c104628ed02b93f1423
asibs <sibsvandal@hotmail.com>
2023-10-29 17:45:28 +0000

    Improve non-live-editing mode - cache file reading

--- components/StoryblokWrapper.tsx
+++ components/StoryblokWrapper.tsx
@@ -1,5 +1,6 @@
 import { ISbStoriesParams, StoryblokComponent, StoryblokStory, getStoryblokApi } from '@storyblok/react/rsc'
 import { readFileSync } from 'fs';
+import { unstable_cache } from 'next/cache';
 import path from 'path';
 
 type params = {
@@ -8,11 +9,11 @@ type params = {
 
 export default async function StoryblokWrapper({slug}: params) {
   if (process.env.ENABLE_STORYBLOK_LIVE_EDITING === 'true') {
-    console.debug('StoryblokWrapper.tsx: Enabling live-editing!')
+    console.debug('StoryblokWrapper.tsx: Enabling live-editing')
+    // When live-editing is enabled, we get data from the Storyblok API
 
-    // We want draft versions of data from Storyblok, as we're live-editing
     let sbParams: ISbStoriesParams = {
-      version: 'draft'
+      version: 'draft' // Want draft versions, as we're live-editing
     }
   
     const storyblokApi = getStoryblokApi()
@@ -22,14 +23,29 @@ export default async function StoryblokWrapper({slug}: params) {
       <StoryblokStory story={storyblokResponse.data.story} />
     )
   } else {
-    console.debug('StoryblokWrapper.tsx: Disabling live-editing!')
+    console.debug('StoryblokWrapper.tsx: Disabling live-editing')
+    // When live-editing is disabled, we use the local filesystem containing the story JSON,
+    // and cache it indefinitely so pages are statically generated at build-time
 
-    const filePath = path.join(process.cwd(), 'storyblok_data', `${slug}.json`);
-    const fileContent = readFileSync(filePath, 'utf8');
-    const data = JSON.parse(fileContent)
-    // const file = await fs.readFile(process.cwd() + `/storyblok_data/${slug}.json`, 'utf8')
-    // const data = JSON.parse(file)
+    // const filePath = path.join(process.cwd(), 'storyblok_data', `${slug}.json`);
+    // const fileContent = readFileSync(filePath, 'utf8');
+    // const data = JSON.parse(fileContent)
 
+    const getLocalStorybookData = unstable_cache(
+      // Cache data function
+      async () => {
+        console.debug(`StoryblokWrapper.tsx: Updating static storyblok data cache for ${slug}`)
+        const filePath = path.join(process.cwd(), 'storyblok_data', `${slug}.json`);
+        const fileContent = readFileSync(filePath, 'utf8');
+        return JSON.parse(fileContent)
+      },
+      // Cache key
+      [`storyblok_data/${slug}.json`],
+      // Cache options
+      { revalidate: false } // Cache will never refresh
+    );
+
+    const data = await getLocalStorybookData();
     return (
       <StoryblokComponent blok={data.story.content} />
     )
