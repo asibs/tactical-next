39963fe45cdb72b2561f0f9697db25e47644f79b
asibs <sibsvandal@hotmail.com>
2023-12-10 15:00:31 +0000

    Remove NavigationLink component (#12)

--- storyblok/components/ComponentsMap.ts
+++ storyblok/components/ComponentsMap.ts
@@ -7,7 +7,6 @@ import Teaser from "@/components/Teaser";
 import Footer from "@/components/footer/Footer";
 import FooterLink from "@/components/footer/FooterLink";
 import Navigation from "@/components/navigation/Navigation";
-import NavigationLink from "@/components/navigation/NavigationLink";
 
 export const ComponentsMap: SbReactComponentsMap = {
   feature: Feature,
@@ -18,5 +17,4 @@ export const ComponentsMap: SbReactComponentsMap = {
   footer_internal_link: FooterLink,
   footer_external_link: FooterLink,
   navbar: Navigation,
-  navbar_link: NavigationLink,
 };
