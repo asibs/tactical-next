2a84fd914e4fc56b7866f05a6e5112b7847cdd52
asibs <sibsvandal@hotmail.com>
2023-12-10 14:08:06 +0000

    Shift various components into subfolders (#11)

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -7,7 +7,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import mvmtFrwdLogo from "../assets/movement-forward-logo-bw.png";
+import mvmtFrwdLogo from "@/assets/movement-forward-logo-bw.png";
 
 import {
   FaDiscord,
--- storyblok/components/ComponentsMap.ts
+++ storyblok/components/ComponentsMap.ts
@@ -4,10 +4,10 @@ import Page from "@/components/Page";
 import Grid from "@/components/Grid";
 import Feature from "@/components/Feature";
 import Teaser from "@/components/Teaser";
-import Footer from "@/components/Footer";
-import Navigation from "@/components/Navigation";
-import NavigationLink from "@/components/NavigationLink";
-import FooterLink from "@/components/FooterLink";
+import Footer from "@/components/footer/Footer";
+import FooterLink from "@/components/footer/FooterLink";
+import Navigation from "@/components/navigation/Navigation";
+import NavigationLink from "@/components/navigation/NavigationLink";
 
 export const ComponentsMap: SbReactComponentsMap = {
   feature: Feature,
