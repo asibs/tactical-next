085de6b959fe7b5a8c20ecba6eab4918ad446e95
asibs <sibsvandal@hotmail.com>
2024-02-17 20:04:15 +0000

    Update postcode lookup form

--- components/postcode/postcodeLookup.tsx
+++ components/postcode/postcodeLookup.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import { Col, Container, Row, Form, Button, FormCheck } from "react-bootstrap";
 import { useFormState, useFormStatus } from "react-dom";
 
 import { parse } from "postcode";
@@ -7,6 +7,10 @@ import sqlite3 from "sqlite3";
 import { open, Database } from "sqlite";
 import path from "path";
 import { redirect } from "next/navigation";
+import FormCheckInput from "react-bootstrap/esm/FormCheckInput";
+import FormCheckLabel from "react-bootstrap/esm/FormCheckLabel";
+
+import rubik from "@/utils/Fonts";
 
 // Let's initialize it as null initially, and we will assign the actual database instance later.
 let db: Database | null = null;
@@ -78,18 +82,64 @@ const initialState: State = {
 const PostcodeLookup = () => {
   // const { pending } = useFormStatus()
   // https://github.com/vercel/next.js/issues/55919
-  // const [state, formAction] = useFormState<State, FormData>(lookupPostcode, initialState)
+  // const [state, formAction] = useFormState<State, FormData>(lookupPostcode, initialFormState)
+
+  {
+    /* useState requires it to be a client-component */
+  }
+  {
+    /* const [emailOptIn, setEmailOptIn] = useState(false); */
+  }
+  const emailOptIn = true;
 
   return (
-    <Container>
+    <Container
+      className="rounded-3 bg-pink-strong p-3 mb-4 shadow text-100"
+      style={{ fontSize: "18px" }}
+    >
       <Form action={lookupPostcode}>
+        <h3 className={`${rubik.className} fw-bolder`}>VOTE THE TORIES OUT</h3>
+        <p className="fw-bold text-900">
+          Vote tactically at the General Election
+        </p>
+
         <Form.Control
           id="postcode"
           name="postcode"
           size="lg"
           type="text"
-          placeholder="Postcode"
+          placeholder="Your Postcode"
         />
+
+        {/* TODO: Show constituency/address drop-down if more than one possibility */}
+
+        {/* Checkbox requires it to be a client-component */}
+        {/* <FormCheck id="email-opt-in">
+          <div onChange={() => setEmailOptIn(!emailOptIn)}>
+            <FormCheckInput checked={emailOptIn} />
+            <FormCheckLabel className="ps-2">
+              <strong>Join with your email</strong> to stick together
+            </FormCheckLabel>
+          </div>
+        </FormCheck> */}
+
+        {emailOptIn && (
+          <>
+            <Form.Control
+              id="email"
+              name="email"
+              size="lg"
+              type="text"
+              placeholder="Your Email"
+            />
+            <p style={{ fontSize: "0.75em" }}>
+              We store your email address, postcode, and constituency, so we can
+              send you exactly the information you need, and the actions to
+              take.
+            </p>
+          </>
+        )}
+
         <Button variant="primary" type="submit" aria-disabled={false}>
           Submit
         </Button>
