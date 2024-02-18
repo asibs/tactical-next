00c96d57aceabdedc41f137f8771bfd2742dc5e4
asibs <sibsvandal@hotmail.com>
2024-02-18 01:36:15 +0000

    Add validation styles. Allow users to select constituency for multi-constituency postcodes.

--- components/postcode/PostcodeLookup.tsx
+++ components/postcode/PostcodeLookup.tsx
@@ -18,15 +18,31 @@ const PostcodeLookup = () => {
   // https://github.com/vercel/next.js/issues/55919
   // const [state, formAction] = useFormState<State, FormData>(lookupPostcode, initialFormState)
 
+  const [errorMessage, setErrorMessage] = useState()
   const [postcode, setPostcode] = useState("");
   const [emailOptIn, setEmailOptIn] = useState(false);
+  const [constituencies, setConstituencies] = useState<Constituency[]>([]);
+  const [constituency, setConstituency] = useState("");
+  const [addresses, setAddresses] = useState<Address[]>([]);
+  const [address, setAddress] = useState("");
   const [email, setEmail] = useState("");
 
   const lookupPostcode = async () => {
     // TODO: check postcode looks valid, check email looks valid, etc
+    // TODO: Subscribe email via ActionNetwork API once we know their constituency
+
+    // User has selected their constituency from drop-down
+    if (constituency) {
+      router.push(`/constituencies/${constituency}`);
+    }
+
+    // 
     const response = await fetch("/api/postcodes", {
       method: "POST",
-      body: `{"postcode": "${postcode}"}`
+      body: JSON.stringify({
+        "postcode": postcode,
+        "address": address,
+      })
     })
 
     const responseJson = await response.json();
@@ -38,6 +54,7 @@ const PostcodeLookup = () => {
       router.push(`/constituencies/${responseJson["constituencies"][0]["slug"]}`);
     } else {
       console.log(responseJson["constituencies"]);
+      setConstituencies(responseJson["constituencies"])
     }
   }
 
@@ -53,17 +70,33 @@ const PostcodeLookup = () => {
         </p>
 
         <Form.Control
-          id="postcode"
           name="postcode"
           size="lg"
           type="text"
           placeholder="Your Postcode"
+          pattern="^\s*[A-Za-z]{1,2}\d[A-Za-z\d]?\s*\d[A-Za-z]{2}\s*$"
           onChange={(e) => setPostcode(e.target.value)}
         />
 
-        {/* TODO: Show constituency/address drop-down if more than one possibility */}
+        {constituencies.length > 0 && (
+          <>
+            <p style={{ fontSize: "0.75em" }}>
+              Sorry, we can't work out exactly which constituency you're in - please select your constituency.
+            </p>
+            <Form.Select
+              name="constituency"
+              size="lg"
+              onChange={(e) => setConstituency(e.target.value)}
+            >
+              <option selected disabled value="" style={{ display: "none" }}>Select Constituency</option>
+              {constituencies.map((c) =>
+                <option value={c.slug}>{c.name}</option>
+              )}
+            </Form.Select>
+          </>
+        )}
 
-        <FormCheck id="email-opt-in">
+        <FormCheck name="emailOptIn">
           <div>
             <FormCheckInput checked={emailOptIn} onChange={() => setEmailOptIn(!emailOptIn)} />
             <FormCheckLabel className="ps-2" onClick={() => setEmailOptIn(!emailOptIn)}>
@@ -72,23 +105,19 @@ const PostcodeLookup = () => {
           </div>
         </FormCheck>
 
-        {emailOptIn && (
-          <>
-            <Form.Control
-              id="email"
-              name="email"
-              size="lg"
-              type="text"
-              placeholder="Your Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <p style={{ fontSize: "0.75em" }}>
-              We store your email address, postcode, and constituency, so we can
-              send you exactly the information you need, and the actions to
-              take.
-            </p>
-          </>
-        )}
+        <Form.Control
+          name="email"
+          size="lg"
+          type="email"
+          placeholder="Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+          hidden={!emailOptIn}
+        />
+        <p style={{ fontSize: "0.75em" }} hidden={!emailOptIn}>
+          We store your email address, postcode, and constituency, so we can
+          send you exactly the information you need, and the actions to
+          take.
+        </p>
 
         <Button variant="primary" type="submit" aria-disabled={false}>
           Submit
