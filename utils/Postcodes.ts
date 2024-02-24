590514394056956cda796edbac5d5cc2573a4a7f
James Southern <jms301@gmail.com>
2024-02-24 11:45:24 +0000

    Fix inputPattern regex missing slashes

--- utils/Postcodes.ts
+++ utils/Postcodes.ts
@@ -1,6 +1,7 @@
 const sanitizePostcode = new RegExp(/[^a-zA-Z0-9]/g);
 const validatePostcode = new RegExp(/^[A-Z]{1,2}\d[A-Z\d]?\d[A-Z]{2}$/);
-const postcodeInputPattern = "^s*[A-Za-z]{1,2}d[A-Za-zd]?s*d[A-Za-z]{2}s*$";
+const postcodeInputPattern =
+  "^\\s*[A-Za-z]{1,2}\\d[A-Za-z\\d]?\\s*\\d[A-Za-z]{2}\\s*$";
 
 const normalizePostcode = (postcode: string) => {
   return postcode.replaceAll(sanitizePostcode, "").toUpperCase();
