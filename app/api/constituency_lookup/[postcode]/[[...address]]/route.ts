5b38b6a128ba47371020478f3631d1eba7a60268
asibs <sibsvandal@hotmail.com>
2024-02-25 10:42:40 +0000

    Remove some logging

--- app/api/constituency_lookup/[postcode]/[[...address]]/route.ts
+++ app/api/constituency_lookup/[postcode]/[[...address]]/route.ts
@@ -1,3 +1,4 @@
+// import * as fs from 'fs';
 import { NextRequest, NextResponse } from "next/server";
 import sqlite3 from "sqlite3";
 import { open, Database } from "sqlite";
@@ -7,27 +8,21 @@ import { validatePostcode, normalizePostcode } from "@/utils/Postcodes";
 // Force using 'nodejs' rather than 'edge' - edge won't have the filesystem containing SQLite
 export const runtime = "nodejs";
 
-const fs = require("fs");
-
 // Declare the DB outside the func & lazy-load, so it can be cached across calls
 let db: Database | null = null;
 const dbPath = path.join(process.cwd(), "data", "postcodes.db");
 console.log(`***** dbPath is [${dbPath}] *****`);
 
 // TODO: Handle errors and return appropriate errorMessage useful for debugging...!
-// TODO: Next.js doesn't cache POSTs - potentially change to a GET of format:
-// constituency_lookup/{postcode}/{addressSlug}
-// which should enable caching
-export async function GET(request: NextRequest, context) {
+export async function GET(request: NextRequest, { params }: { params: { postcode: string, address: string[] } }) {
   console.log("IN SERVER FUNCTION");
-  console.log(request);
-  const postcode = context.params.postcode;
-  const addressSlug = context.params?.address?.[0];
+  const postcode = params.postcode;
+  const addressSlug = params.address?.[0];
 
-  console.log("***** FILES IN DATA DIRECTORY *****");
-  fs.readdirSync(path.join(process.cwd(), "data")).forEach((file: any) => {
-    console.log(file);
-  });
+  // console.log("***** FILES IN DATA DIRECTORY *****");
+  // fs.readdirSync(path.join(process.cwd(), "data")).forEach((file: any) => {
+  //   console.log(`- ${file}`);
+  // });
 
   //read & normalize postcode
   const normalizedPostcode = normalizePostcode(postcode);
@@ -67,8 +62,6 @@ export async function GET(request: NextRequest, context) {
     normalizedPostcode,
   );
   console.timeEnd("query-postcode-database");
-
-  console.log(`Found constituencues ${constituencies} from database`);
 
   if (!constituencies || constituencies.length == 0) {
     console.log(`Postcode ${normalizedPostcode} not found in DB!`);
