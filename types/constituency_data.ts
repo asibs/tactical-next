d3ee5ba8f2012246b49037b62b6b7c02858d89ae
asibs <sibsvandal@hotmail.com>
2024-02-25 14:11:45 +0000

    Convert vote percents to numbers

--- types/constituency_data.ts
+++ types/constituency_data.ts
@@ -1,3 +1,4 @@
+// Top-level type for all constituency data, which we use to statically generate our constituency pages
 type ConstituencyData = {
   constituencyIdentifiers: ConstituencyIdentifiers;
   recommendation: Recommendation;
@@ -5,22 +6,33 @@ type ConstituencyData = {
   pollingResults: VoteResult;
 };
 
+// Strings used to identify constituencies
 type ConstituencyIdentifiers = {
   slug: string;
   name: string;
   mySocietyCode: string;
 };
 
+// Data about the tactical recommendation we're using
 type Recommendation = {
   partySlug: string;
   reason: string;
 };
 
+// An election "result" - note this encompasses:
+// - Real results - an actual election happened with the same boundary
+// - Implied - an election happened, but with different boundaries, so a calculation has been done
+//   to work out the _implied_ result with the new boundaries
+// - Predicted - polling carried out gives us an indicative idea of the result of an upcoming
+//   election
 type VoteResult = {
   partyVoteResults: VoteResult[];
+  // In future we may want to add other data like turnout
 };
 
+// The vote a single party achieved in a given VoteResult
 type PartyVoteResult = {
   partySlug: string;
   votePercent: number;
+  // In future we might want to add other data like actual vote count, candidate name(s), etc
 };
