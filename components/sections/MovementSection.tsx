489c66e27529b50f556fb047d716ec69c47a097f
asibs <sibsvandal@hotmail.com>
2024-03-02 20:12:44 +0000

    lint/pretty

--- components/sections/MovementSection.tsx
+++ components/sections/MovementSection.tsx
@@ -17,47 +17,57 @@ import seanAdams from "@/assets/supporter_portraits/sean-adams-540.webp";
 import supertanskiii from "@/assets/supporter_portraits/supertanskiii-540.webp";
 import { StaticImageData } from "next/image";
 
-
-
 const MovementSection = () => {
   const people = {
-    "Asif Kapadia": { "image": asifKapadia },
-    "Carol Vorderman": { "image": carolVorderman },
-    "Danny Price": { "image": dannyPrice },
-    "Emma Kennedy": { "image": emmaKennedy },
-    "Femi Oluwole": { "image": femiOluwole },
-    "Graham Hughes": { "image": grahamHughes },
-    "Patsy Stevenson": { "image": patsyStevenson },
-    "Sean Adams": { "image": seanAdams },
-    "Liz Webster": { "image": lizWebster },
-    "Marina Purkiss": { "image": marinaPurkiss },
-    "Supertanskiii": { "image": supertanskiii },
-    "Josh Russell": { "image": joshRussell }
-  }
+    asifKapadia: { name: "Asif Kapadia", image: asifKapadia },
+    carolVorderman: { name: "Carol Vorderman", image: carolVorderman },
+    dannyPrice: { name: "Danny Price", image: dannyPrice },
+    emmaKennedy: { name: "Emma Kennedy", image: emmaKennedy },
+    femiOluwole: { name: "Femi Oluwole", image: femiOluwole },
+    grahamHughes: { name: "Graham Hughes", image: grahamHughes },
+    patsyStevenson: { name: "Patsy Stevenson", image: patsyStevenson },
+    seanAdams: { name: "Sean Adams", image: seanAdams },
+    lizWebster: { name: "Liz Webster", image: lizWebster },
+    marinaPurkiss: { name: "Marina Purkiss", image: marinaPurkiss },
+    supertanskiii: { name: "Supertanskiii", image: supertanskiii },
+    joshRussell: { name: "Josh Russell", image: joshRussell },
+  };
 
   return (
     <section id="section-movement" className="section-light">
       <Container className="py-4 py-md-5">
-
         {/* PEOPLE */}
         <Row>
           <Col className="pb-3 pb-md-5">
-            <h2 className={`${rubik.className} fw-bolder fs-1`}>Join A Movement building voter power, beyond this election.</h2>
+            <h2 className={`${rubik.className} fw-bolder fs-1`}>
+              Join A Movement building voter power, beyond this election.
+            </h2>
           </Col>
         </Row>
 
-        <Row xs={4} md={6} className="g-0 mb-4 mb-md-5" style={{ margin: "0px calc(50% - 50vw)", width: "100vw" }}>
-          {Object.keys(people).map((name) => {
-            // TS isn't smart enough to work out name is just the keys from the people object,
-            // so thinks name might not be a valid key...
+        <Row
+          xs={4}
+          md={6}
+          className="g-0 mb-4 mb-md-5"
+          style={{ margin: "0px calc(50% - 50vw)", width: "100vw" }}
+        >
+          {Object.keys(people).map((key) => {
+            // TS isn't smart enough to work out `key` is just the keys from the people object,
+            // so thinks they might not be a valid key for the object...
             // @ts-expect-error
-            const image: StaticImageData = people[name]["image"];
+            const name: string = people[key]["name"];
+            // @ts-expect-error
+            const image: StaticImageData = people[key]["image"];
 
             return (
-              <Col>
-                <Image src={image} alt={`Photo of ${name}`} style={{ width: "100%", height: "100%" }} />
+              <Col key={key}>
+                <Image
+                  src={image}
+                  alt={`Photo of ${name}`}
+                  style={{ width: "100%", height: "100%" }}
+                />
               </Col>
-            )
+            );
           })}
         </Row>
 
@@ -65,13 +75,27 @@ const MovementSection = () => {
         <Row xs={1} md={2}>
           <Col>
             <h3 className={`${rubik.className} fw-bolder fs-5`}>We show up</h3>
-            <h4 className={`${rubik.className} fw-bolder fs-2`}>To get the tories out</h4>
-            <p>In many places around the country, if we vote together for the most likely candidate to beat the Tory, out votes demolish them into tiny numbers.</p>
+            <h4 className={`${rubik.className} fw-bolder fs-2`}>
+              To get the tories out
+            </h4>
+            <p>
+              In many places around the country, if we vote together for the
+              most likely candidate to beat the Tory, out votes demolish them
+              into tiny numbers.
+            </p>
           </Col>
           <Col>
-            <h3 className={`${rubik.className} fw-bolder fs-5`}>We stick together</h3>
-            <h4 className={`${rubik.className} fw-bolder fs-2`}>To influence the next government</h4>
-            <p>A bigger influence on the next government than the media that currently shape the narrative. We stick together offering our votes for the changes we want.</p>
+            <h3 className={`${rubik.className} fw-bolder fs-5`}>
+              We stick together
+            </h3>
+            <h4 className={`${rubik.className} fw-bolder fs-2`}>
+              To influence the next government
+            </h4>
+            <p>
+              A bigger influence on the next government than the media that
+              currently shape the narrative. We stick together offering our
+              votes for the changes we want.
+            </p>
           </Col>
         </Row>
       </Container>
