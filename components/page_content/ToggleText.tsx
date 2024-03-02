c43bf92fb605668099ad8ae3650ad2d31c2f49ae
asibs <sibsvandal@hotmail.com>
2024-03-02 22:41:07 +0000

    Improve style of ToggleText storyblok component

--- components/page_content/ToggleText.tsx
+++ components/page_content/ToggleText.tsx
@@ -5,7 +5,7 @@ import { ToggleTextStoryblok } from "@/storyblok/types/storyblok-types";
 import { storyblokEditable } from "@storyblok/react/rsc";
 import { render } from "storyblok-rich-text-react-renderer";
 import { useState } from "react";
-import { Collapse } from "react-bootstrap";
+import { Col, Collapse, Container, Row } from "react-bootstrap";
 import { FaCircleChevronRight } from "react-icons/fa6";
 
 import { rubik } from "@/utils/Fonts";
@@ -14,7 +14,44 @@ const ToggleText = ({ blok }: { blok: ToggleTextStoryblok }) => {
   const [visible, setVisible] = useState(blok.text_shown_initially);
 
   return (
-    <div {...storyblokEditable(blok)} className="text-start">
+    <div {...storyblokEditable(blok)} className="my-4">
+      <Container>
+        <Row>
+          <Col xs="auto" className="g-0">
+            <FaCircleChevronRight
+              onClick={() => setVisible(!visible)}
+              aria-controls={`collapse-${blok._uid}`}
+              aria-expanded={visible}
+              className="fs-2"
+              style={{
+                transition: "transform 0.3s ease 0s",
+                transform: `rotate(${visible ? 90 : 0}deg)`,
+                cursor: "pointer"
+              }}
+            />
+          </Col>
+          <Col>
+            <Row
+              onClick={() => setVisible(!visible)}
+              aria-controls={`collapse-${blok._uid}`}
+              aria-expanded={visible}
+              style={{ cursor: "pointer" }}
+            >
+              <h2 className={rubik.className}>
+                {blok.title}
+              </h2>
+            </Row>
+            <Row>
+              <Collapse in={visible}>
+                <div id={`collapse-${blok._uid}`}>{render(blok.text)}</div>
+              </Collapse>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+
+
+      {/*
       <div
         onClick={() => setVisible(!visible)}
         aria-controls={`collapse-${blok._uid}`}
@@ -35,8 +72,9 @@ const ToggleText = ({ blok }: { blok: ToggleTextStoryblok }) => {
       </div>
 
       <Collapse in={visible}>
-        <div id={`collapse-${blok._uid}`}>{render(blok.text)}</div>
+        <div id={`collapse-${blok._uid}`} className="ms-5">{render(blok.text)}</div>
       </Collapse>
+      */}
     </div>
   );
 };
