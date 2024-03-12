eae544a9c2a67271080b354cf21f6484520adb61
asibs <sibsvandal@hotmail.com>
2024-03-12 00:17:13 +0000

    Add postcode lookup attribution to footer (#61)
    * Add attribution to footer
    
    * Update style, fix u in p issue

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -27,129 +27,188 @@ import { rubik } from "@/utils/Fonts";
 
 const Footer = ({ blok }: { blok: FooterStoryblok }) => (
   <footer {...storyblokEditable(blok)}>
-    <Container>
-      <Row>
-        {/* FOOTER CALL TO ACTION BUTTONS */}
-        <Col sm={10} md={6} lg={4} xl={4} className="pb-4">
-          <ButtonGroup vertical size="lg" className="w-100">
-            <Link
-              href="/reminders"
-              className={`btn btn-dark btn-lg ${rubik.className} fw-bolder text-start lh-sm mb-1`}
-            >
-              <FaBell className="me-2" />
-              Join the voting movement
-            </Link>
-            <Link
-              href="/constituencies"
-              className={`btn btn-dark btn-lg ${rubik.className} fw-bolder text-start lh-sm mb-1`}
-            >
-              <FaMagnifyingGlass className="me-2" />
-              Browse constituencies
-            </Link>
+    <section className="footer">
+      <Container>
+        <Row>
+          {/* FOOTER CALL TO ACTION BUTTONS */}
+          <Col sm={10} md={6} lg={4} xl={4} className="pb-4">
+            <ButtonGroup vertical size="lg" className="w-100">
+              <Link
+                href="/reminders"
+                className={`btn btn-dark btn-lg ${rubik.className} fw-bolder text-start lh-sm mb-1`}
+              >
+                <FaBell className="me-2" />
+                Join the voting movement
+              </Link>
+              <Link
+                href="/constituencies"
+                className={`btn btn-dark btn-lg ${rubik.className} fw-bolder text-start lh-sm mb-1`}
+              >
+                <FaMagnifyingGlass className="me-2" />
+                Browse constituencies
+              </Link>
+              <a
+                href="https://themovementforward.com/volunteer/"
+                target="_blank"
+                rel="noreferrer"
+                className={`btn btn-dark btn-lg ${rubik.className} fw-bolder text-start lh-sm mb-1`}
+              >
+                <FaPuzzlePiece className="me-2" />
+                Volunteer
+              </a>
+              <Link
+                href="/downloads"
+                className={`btn btn-dark btn-lg ${rubik.className} fw-bolder text-start lh-sm mb-1`}
+              >
+                <FaFileArrowDown className="me-2" />
+                Downloads and resources
+              </Link>
+            </ButtonGroup>
+          </Col>
+
+          {/* QR CODE */}
+          <Col sm={10} md={5} lg={3} xl={4} className="pb-4 px-xl-5">
             <a
-              href="https://themovementforward.com/volunteer/"
+              href="https://shop.mvtfwd.com/stickers"
               target="_blank"
               rel="noreferrer"
-              className={`btn btn-dark btn-lg ${rubik.className} fw-bolder text-start lh-sm mb-1`}
             >
-              <FaPuzzlePiece className="me-2" />
-              Volunteer
-            </a>
-            <Link
-              href="/downloads"
-              className={`btn btn-dark btn-lg ${rubik.className} fw-bolder text-start lh-sm mb-1`}
-            >
-              <FaFileArrowDown className="me-2" />
-              Downloads and resources
-            </Link>
-          </ButtonGroup>
-        </Col>
-
-        {/* QR CODE */}
-        <Col sm={10} md={5} lg={3} xl={4} className="pb-4 px-xl-5">
-          <a
-            href="https://shop.mvtfwd.com/stickers"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Image
-              src={mvmtFrwdQrCode}
-              alt="Movement Forward QR Code"
-              style={{ width: "100%", height: "auto" }}
-              placeholder="blur"
-            />
-          </a>
-        </Col>
-
-        <Col sm={10} md={10} lg={5} xl={4} className="pb-4">
-          {/* MOVEMENT FORWARD LOGO */}
-          <p>
-            <a href="https://mvtfwd.com/links" target="_blank" rel="noreferrer">
               <Image
-                src={mvmtFrwdLogo}
-                alt="Movement Forward Logo"
-                style={{ width: "4rem", height: "4rem" }}
+                src={mvmtFrwdQrCode}
+                alt="Movement Forward QR Code"
+                style={{ width: "100%", height: "auto" }}
                 placeholder="blur"
               />
-              <span className="mx-2 fw-bold">@MVTFWD</span>
             </a>
-          </p>
+          </Col>
 
-          {/* MOVEMENT FORWARD SOCIAL LINKS */}
-          <ul className="list-inline my-3">
-            <li className="list-inline-item">
+          <Col sm={10} md={10} lg={5} xl={4} className="pb-4">
+            {/* MOVEMENT FORWARD LOGO */}
+            <p>
               <a
-                href="https://twitter.com/mvtfwd"
+                href="https://mvtfwd.com/links"
                 target="_blank"
                 rel="noreferrer"
               >
-                <FaSquareTwitter className="fs-1" />
+                <Image
+                  src={mvmtFrwdLogo}
+                  alt="Movement Forward Logo"
+                  style={{ width: "4rem", height: "4rem" }}
+                  placeholder="blur"
+                />
+                <span className="mx-2 fw-bold">@MVTFWD</span>
               </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="https://facebook.com/mvtfwd" target="_blank">
-                <FaSquareFacebook className="fs-1" />
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="https://instagram.com/mvtfwd" target="_blank">
-                <FaSquareInstagram className="fs-1" />
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="https://threads.net/mvtfwd" target="_blank">
-                <FaSquareThreads className="fs-1" />
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="https://mvtfwd.com/whatsapp" target="_blank">
-                <FaSquareWhatsapp className="fs-1" />
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="https://mvtfwd.com/volunteer" target="_blank">
-                <FaDiscord className="fs-1" />
-              </a>
-            </li>
-          </ul>
+            </p>
 
-          {/* COPYRIGHT */}
-          <p className="my-3">
-            A Movement Forward initiative
-            <br />© 2024 Forward Democracy Limited
-          </p>
+            {/* MOVEMENT FORWARD SOCIAL LINKS */}
+            <ul className="list-inline my-3">
+              <li className="list-inline-item">
+                <a
+                  href="https://twitter.com/mvtfwd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaSquareTwitter className="fs-1" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://facebook.com/mvtfwd" target="_blank">
+                  <FaSquareFacebook className="fs-1" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://instagram.com/mvtfwd" target="_blank">
+                  <FaSquareInstagram className="fs-1" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://threads.net/mvtfwd" target="_blank">
+                  <FaSquareThreads className="fs-1" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://mvtfwd.com/whatsapp" target="_blank">
+                  <FaSquareWhatsapp className="fs-1" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://mvtfwd.com/volunteer" target="_blank">
+                  <FaDiscord className="fs-1" />
+                </a>
+              </li>
+            </ul>
 
-          {/* FOOTER LINKS */}
-          <Row xs={4} className="py-3">
-            {blok.links.map((link) => (
-              <Col className="p-1" key={link._uid}>
-                <StoryblokComponent blok={link} />
-              </Col>
-            ))}
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+            {/* COPYRIGHT */}
+            <p className="my-3">
+              A Movement Forward initiative
+              <br />© 2024 Forward Democracy Limited
+            </p>
+
+            {/* FOOTER LINKS */}
+            <Row xs={4} className="py-3">
+              {blok.links.map((link) => (
+                <Col className="p-1" key={link._uid}>
+                  <StoryblokComponent blok={link} />
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section className="footer-dark" style={{ fontSize: "14px" }}>
+      <Container>
+        <Row>
+          <Col xs="auto">
+            {/* POSTCODE LOOKUP ATTRIBUTION */}
+            <p className="mb-1">
+              Postcode lookup contains data from{" "}
+              <a
+                href="https://democracyclub.org.uk/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                DemocracyClub
+              </a>
+              , the{" "}
+              <a
+                href="https://geoportal.statistics.gov.uk/datasets/3700342d3d184b0d92eae99a78d9c7a3/about"
+                target="_blank"
+                rel="noreferrer"
+              >
+                ONS
+              </a>{" "}
+              &{" "}
+              <a
+                href="https://pages.mysociety.org/2025-constituencies/datasets/parliament_con_2025/0_1_4"
+                target="_blank"
+                rel="noreferrer"
+              >
+                MySociety
+              </a>
+            </p>
+            <ul>
+              <li>
+                Contains OS data © Crown copyright and database right 2024
+              </li>
+              <li>
+                Contains Royal Mail data © Royal Mail copyright and Database
+                right 2024
+              </li>
+              <li>
+                Contains GeoPlace data © Local Government Information House
+                Limited copyright and database right 2024
+              </li>
+              <li>
+                Source: Office for National Statistics licensed under the Open
+                Government Licence v.3.0
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   </footer>
 );
 
