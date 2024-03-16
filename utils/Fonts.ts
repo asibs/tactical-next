caa7f521b251be2e3d35353d7540c049e3198c82
asibs <sibsvandal@hotmail.com>
2024-03-16 13:43:57 +0000

    Delete unnecessary Inter font

--- utils/Fonts.ts
+++ utils/Fonts.ts
@@ -1,4 +1,4 @@
-import { Rubik, Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 
 const rubik = Rubik({
   subsets: ["latin"],
@@ -6,7 +6,4 @@ const rubik = Rubik({
   variable: "--font-rubik",
 });
 
-// TODO: Decide whether we want Inter at all or just use default fonts?
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-
-export { rubik, inter };
+export { rubik };
