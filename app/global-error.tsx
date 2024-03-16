57c70183107cead57dda04e5dfb62602d497db49
asibs <sibsvandal@hotmail.com>
2024-03-16 15:28:42 +0000

    Error pages (#69)
    * Standard error page
    
    * temporary page to test error handling works on Prod as expected
    
    * Prettier fix
    
    * Global fallback error page
    
    * Temp error to test global fallback
    
    * Improve global error layout
    
    * Remove temp nav error used to test global-error on Prod
    
    * Minor style improvements
    
    * Delete temp error page

---
+++ app/global-error.tsx
@@ -0,0 +1,74 @@
+"use client"; // Error components must be Client Components
+
+import Link from "next/link";
+
+import { useEffect } from "react";
+
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+import { FaArrowRotateRight } from "react-icons/fa6";
+import { rubik } from "@/utils/Fonts";
+
+// Global fallback error boundary. See:
+// https://nextjs.org/docs/app/building-your-application/routing/error-handling#handling-errors-in-root-layouts
+//
+// No layout is rendered in this view, so we need to render everything from scratch.
+//
+// If we've hit this global fallback, rather than the root error.tsx, it probably means
+// something is wrong with one of our common components used in the root layout (navbar,
+// footer, etc), so probably safest NOT to render any of these...!
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <html lang="en" className={rubik.variable}>
+      <body>
+        <header className="bg-black text-white">
+          <Container className="py-4 py-md-6">
+            <h2>Oops, something went wrong</h2>
+          </Container>
+        </header>
+
+        <main>
+          <section className="section-light">
+            <Container className="pb-5">
+              <Row className="pb-5">
+                <Col className="text-center">
+                  <Button
+                    size="lg"
+                    variant="dark"
+                    className="fw-bolder px-5"
+                    onClick={
+                      // Attempt to recover by trying to re-render the segment
+                      () => reset()
+                    }
+                  >
+                    <FaArrowRotateRight className="me-2" />
+                    Try Again
+                  </Button>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col className="text-center">
+                  <h4>
+                    <Link href="/">Back to Stop The Tories homepage</Link>
+                  </h4>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </main>
+      </body>
+    </html>
+  );
+}
