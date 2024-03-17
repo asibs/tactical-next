f7a8e7c7edb206b57da351953a9d4a062a02e5a5
asibs <sibsvandal@hotmail.com>
2024-03-17 19:32:22 +0000

    Add James to Team page

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,4 +1,6 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+
+import React from "react";
 
 import { Col, Container, Row } from "react-bootstrap";
 
@@ -6,6 +8,55 @@ import Header from "@/components/Header";
 
 import josh from "@/assets/team_portraits/josh.webp";
 import andrew from "@/assets/team_portraits/andrew.webp";
+import james from "@/assets/team_portraits/james.webp";
+
+function TeamMember({
+  children,
+  imgSrc,
+  imgAlt,
+  align,
+}: {
+  children: React.ReactNode;
+  imgSrc: StaticImageData;
+  imgAlt: string;
+  align: "left" | "right";
+}) {
+  const image = (
+    <Image
+      src={imgSrc}
+      alt={imgAlt}
+      sizes="100%"
+      style={{
+        position: "relative",
+        height: "100%",
+        width: "100%",
+        objectFit: "contain",
+        borderRadius: "50%",
+      }}
+      placeholder="blur"
+    />
+  );
+
+  if (align == "left") {
+    return (
+      <Row className="pb-5 align-items-start align-items-md-center">
+        <Col xs={4} md={3} lg={2}>
+          {image}
+        </Col>
+        <Col className="text-start">{children}</Col>
+      </Row>
+    );
+  } else {
+    return (
+      <Row className="pb-5 align-items-start align-items-md-center">
+        <Col className="text-end">{children}</Col>
+        <Col xs={4} md={3} lg={2}>
+          {image}
+        </Col>
+      </Row>
+    );
+  }
+}
 
 export default function TeamPage() {
   return (
@@ -19,55 +70,59 @@ export default function TeamPage() {
       <main>
         <section className="section-light">
           <Container>
-            <Row className="pb-5 align-items-start align-items-md-center">
-              <Col xs={4} md={3} lg={2}>
-                <Image
-                  src={josh}
-                  alt="Photo of Josh Russell"
-                  sizes="100%"
-                  style={{
-                    position: "relative",
-                    height: "100%",
-                    width: "100%",
-                    objectFit: "contain",
-                    borderRadius: "50%",
-                  }}
-                  placeholder="blur"
-                />
-              </Col>
-              <Col>
-                <h3>Josh Russell</h3>
-                <p>Our Lord & saviour.</p>
-              </Col>
-            </Row>
+            <TeamMember
+              imgSrc={josh}
+              imgAlt="Photo of Josh Russel"
+              align="left"
+            >
+              <h3>
+                <a
+                  href="https://twitter.com/JoshFwd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Josh Russell
+                </a>
+              </h3>
+              <p>Our Lord & saviour.</p>
+            </TeamMember>
 
-            <Row className="pb-5 align-items-start align-items-md-center">
-              <Col className="text-end">
-                <h3>Andrew Sibley</h3>
-                <p>
-                  Software developer with experience building tech for political
-                  & social campaigning.
-                  <br />
-                  Has voted for all the major progressive parties in England at
-                  one point or another.
-                </p>
-              </Col>
-              <Col xs={4} md={3} lg={2}>
-                <Image
-                  src={andrew}
-                  alt="Photo of Andrew Sibley"
-                  sizes="100%"
-                  style={{
-                    position: "relative",
-                    height: "100%",
-                    width: "100%",
-                    objectFit: "contain",
-                    borderRadius: "50%",
-                  }}
-                  placeholder="blur"
-                />
-              </Col>
-            </Row>
+            <TeamMember
+              imgSrc={andrew}
+              imgAlt="Photo of Andrew Sibley"
+              align="right"
+            >
+              <h3>Andrew Sibley</h3>
+              <p>
+                Software developer with experience building tech for political &
+                social campaigning.
+                <br />
+                Has voted for all the major progressive parties in England at
+                one point or another.
+              </p>
+            </TeamMember>
+
+            <TeamMember
+              imgSrc={james}
+              imgAlt="Photo of James Southern"
+              align="left"
+            >
+              <h3>
+                <a
+                  href="https://twitter.com/Super_James"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  James Southern
+                </a>
+              </h3>
+              <p>
+                Recently head of strategy at the Make Votes Matter campaign for
+                electoral reform.
+                <br />A keen sea kayaker, James has a background in software
+                development from fin-tech to museum exhibits.
+              </p>
+            </TeamMember>
           </Container>
         </section>
       </main>
