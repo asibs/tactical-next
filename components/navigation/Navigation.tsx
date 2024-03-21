16efd7f0ce849a5f1db56d3a9970fb318249bf34
James Southern <jms301@gmail.com>
2024-03-21 11:13:35 +0000

    update the navbar to use Josh's bootstrap layout

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -3,9 +3,8 @@
 // (even tho this navbar doesn't use a collapse, it still fails without use client)
 
 import Image from "next/image";
-import Link from "next/link";
 
-import { Container, Navbar, Row, Col } from "react-bootstrap";
+import { Container, Button, Nav, Navbar } from "react-bootstrap";
 
 import logo from "@/assets/stop-the-tories-logo-transparent.png";
 
@@ -19,55 +18,50 @@ import styles from "./styles.module.css";
 // menu. Users need to scroll to the footer to see links to non-primary pages.
 const Navigation = () => {
   return (
-    <Navbar
-      bg="light"
-      data-bs-theme="light"
-      sticky="top"
-      className={rubik.className}
-    >
-      <Container className="p-2">
-        {/* Branding section - left-aligned */}
-        <Navbar.Brand as={Link} href="/">
-          <Image
-            src={logo}
-            alt="StopTheTories.vote logo"
-            className="d-inline-block me-2"
-            style={{ width: "2rem", height: "2rem" }}
-            placeholder="blur"
-          />
-          <span className="brand-text">StopTheTories</span>
-          <span className="brand-text fw-bolder fst-italic">.Vote</span>
-        </Navbar.Brand>
+    <Container fluid className="bg-light sticky-top">
+      <Navbar className={"py-3 " + rubik.className}>
+        <Container className="px-0">
+          {/* Branding section - left-aligned */}
+          <Navbar.Brand href="/" className="d-flex align-items-center">
+            <span className="bs-icon-sm d-flex justify-content-center align-items-center me-2 bs-icon">
+              <Image
+                src={logo}
+                alt="StopTheTories.vote logo"
+                style={{ width: "2rem", height: "2rem" }}
+                placeholder="blur"
+              />
+            </span>
+            <span className="brand-text">StopTheTories</span>
+            <span className="brand-text fw-bolder fst-italic">.Vote</span>
+          </Navbar.Brand>
 
-        {/* Right-aligned section - always shown links */}
-        <div>
-          <Container className="p-0">
-            <Row xs="auto" className="g-1 g-sm-2 g-lg-3">
-              <Col>
-                <Link
-                  className="btn btn-dark fw-bolder px-2 px-sm-3"
-                  role="button"
-                  href="/"
-                >
-                  <FaMagnifyingGlass className="me-0 me-sm-2" />
-                  <span className="d-none d-sm-inline-block">Search</span>
-                </Link>
-              </Col>
-              <Col>
-                <Link
-                  className="btn btn-pink-strong text-white fw-bolder px-2 px-sm-3"
-                  role="button"
-                  href="/reminders"
-                >
-                  <FaBoltLightning className="d-none d-sm-inline-block me-2" />
-                  <span>Join</span>
-                </Link>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </Container>
-    </Navbar>
+          {/* Right-aligned section - always shown links */}
+          <Navbar.Collapse>
+            {/* pushes the buttons to the right */}
+            <Nav as="ul" className="ms-auto"></Nav>
+            <Button
+              variant="dark"
+              href="/"
+              className="d-md-none d-inline-block"
+            >
+              <FaMagnifyingGlass className="mx-0" />
+            </Button>
+            <Button
+              variant="dark"
+              href="/"
+              className="d-none d-md-inline-block"
+            >
+              <FaMagnifyingGlass />
+              Search
+            </Button>
+            <Button href="/reminders" className="ms-2">
+              <FaBoltLightning className="d-none d-md-inline-block" />
+              Join
+            </Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
